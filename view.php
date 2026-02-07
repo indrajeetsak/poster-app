402e07fcf181b70919adc8b63745f81fb5e2a397
indrajeetsak <indrajeetsak@gmail.com>
2026-02-07 14:37:14 +0530

    First draft

--- view.php
+++ view.php
@@ -55,6 +55,7 @@ if ($id) {
     
     <div class="bg-white rounded-lg shadow-xl max-w-2xl w-full overflow-hidden text-center p-2">
         <h3 class="animate-pulse text-xl font-bold mb-6 text-red-800"><?php echo $pageTitle; ?></h1>
+        <h3 class="animate-bounce text-xl font-bold mb-6 text-red-800">👇👇👇👇👇👇👇</h1>
         
         <?php if ($imageUrl): ?>
             <img src="<?php echo $imageUrl; ?>" alt="Generated Poster" class="w-full h-auto max-w-full rounded-lg shadow-md mb-8">
