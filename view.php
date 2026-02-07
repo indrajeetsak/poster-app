972d88fef616b45d86eff45529080430a714a05f
indrajeetsak <indrajeetsak@gmail.com>
2026-02-07 13:29:14 +0530

    First draft

--- view.php
+++ view.php
@@ -53,7 +53,7 @@ if ($id) {
 </head>
 <body class="min-h-screen flex flex-col items-center justify-center">
     
-    <div class="bg-white rounded-lg shadow-xl max-w-2xl w-full overflow-hidden text-center p-6">
+    <div class="bg-white rounded-lg shadow-xl max-w-2xl w-full overflow-hidden text-center p-2">
         <h3 class="text-xl font-bold mb-6 text-gray-800"><?php echo $pageTitle; ?></h1>
         
         <?php if ($imageUrl): ?>
